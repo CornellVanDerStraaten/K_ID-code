453d8f884522638c60dbc7e972ccdcac18a7c076
Cornell van der Straaten <30183@ma-web.nl>
2020-04-20 11:45:03 +0200

    modal

---
+++ definey/script.js
@@ -0,0 +1,9 @@
+const modaal = document.getElementById('modaal');
+
+function registerDisplay() {
+  modaal.style.display = 'block'
+}
+
+function closeRegisterDisplay() {
+    modaal.style.display = 'none'
+}
