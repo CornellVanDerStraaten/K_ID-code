eead84bdf8d6b7a6fb66db49bdc61b14aba622b3
Cornell van der Straaten <30183@ma-web.nl>
2020-04-21 11:06:34 +0200

    week page done

--- definey/script.js
+++ definey/script.js
@@ -1,7 +1,8 @@
 const modaal = document.getElementById('modaal');
+const inModaal = document.getElementById('inModaal');
 
 function registerDisplay() {
-  modaal.style.display = 'block'
+  modaal.style.display = 'block';
 }
 
 function closeRegisterDisplay() {
